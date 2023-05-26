35157ab88aac3095c4344d9d54564c616687a518
SimonNyvall <simon.k.nyvall@gmail.com>
2023-05-27 01:25:03 +0200

    Added back the tests

--- DotCon.Tests/Terminal_Tests.cs
+++ DotCon.Tests/Terminal_Tests.cs
@@ -6,15 +6,56 @@ namespace DotCon.Tests;
 public class Terminal_Tests
 {
     [Fact]
-    public void Test1()
+    public void Run_ReturnsHelloWorld_WithInputEchoHelloWorld()
     {
         // Arrange
-        var bash = Terminal.UseBashShell();
+        var terminal = Terminal.UseBashShell();
+
+        // Act
+        var result = terminal.Run("echo 'HelloWorld'");
+
+        // Assert
+        Assert.Equal("HelloWorld\n", result);
+    }
+
+    [Fact]
+    public void TryRun_ReturnsHelloWorld_WithInputEchoHelloWorld()
+    {
+        // Arrange
+        var terminal = Terminal.UseBashShell();
+
+        // Act
+        var result = terminal.TryRun("echo 'HelloWorld'", out var output);
+
+        // Assert
+        Assert.Equal("HelloWorld\n", output);
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public async Task RunAsync_ReturnsHelloWorld_WithInputEchoHelloWorld()
+    {
+        // Arrange
+        var terminal = Terminal.UseBashShell();
+
+        // Act
+        var result = await terminal.RunAsync("echo 'HelloWorld'");
+
+        // Assert
+        Assert.Equal("HelloWorld\n", result);
+    }
+    
+    [Fact]
+    public void Execute_ReturnsHelloWorld_WithInputEchoHelloWorld()
+    {
+        // Arrange
+        var terminal = Terminal.UseBashShell();
+        terminal.StoreScript("echo", "echo 'HelloWorld'");
         
         // Act
-        var result = bash.Run("echo 'Hello World!'");
-        
+        var result = terminal.ExecuteScript("echo");
+
         // Assert
-        Assert.Equal("Hello World!\n", result);
+        Assert.Equal("HelloWorld\n", result);
     }
 }
